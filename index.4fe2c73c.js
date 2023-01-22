f8e061aed4aaeb0782e68a5b948923c503a7bc09
Voff4eg1905 <106484238+Voff4eg1905@users.noreply.github.com>
2023-01-22 16:20:26 +0000

    Deploying to gh-pages from @ Voff4eg1905/goit-js-hw-10@ede4162b75aeb4ce88ea5011da935fd6c0c678f6 🚀

---
+++ index.4fe2c73c.js
@@ -0,0 +1,2 @@
+document.querySelector("#search-box");console.log("Hello");
+//# sourceMappingURL=index.4fe2c73c.js.map
---
+++ index.742dac36.js
@@ -0,0 +1,2 @@
+!function(){document.querySelector("#search-box");console.log("Hello")}();
+//# sourceMappingURL=index.742dac36.js.map
