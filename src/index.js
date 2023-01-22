1c1d8aad1d92995721d55875e3eda10282e7b05f
Voff4eg1905 <106484238+Voff4eg1905@users.noreply.github.com>
2023-01-22 19:51:27 +0200

    in progress

--- src/index.js
+++ src/index.js
@@ -1,5 +1,26 @@
-import './css/styles.css';
-
+// import './css/styles.css';
+var debounce = require('lodash.debounce')
+import {fetchCountries} from './fetchCountries';
 const DEBOUNCE_DELAY = 300;
-const inputEl = document.querySelector("#search-box");
-console.log("Hello");
+const inputEl = document.querySelector('#search-box');
+const countryInfoEl = document.querySelector(".country-info")
+
+const onInputEntry = event => {
+  let enteredText = event.target.value.trim();
+  if (!enteredText) {
+    return;
+  }
+  console.log(enteredText);
+  fetchCountries(enteredText).then(data => { console.log(data);  renderCountry(data)}).catch(err => console.log(err))
+};
+inputEl.addEventListener('input', debounce(onInputEntry, DEBOUNCE_DELAY) );
+
+
+    
+
+function renderCountry (country) {
+return countryInfoEl.innerHTML = ` <img src="${country[0].flags.svg}" alt=""> <h1>${country[0].name}</h1>
+<p>Capital: ${country[0].capital} </p>
+<p>Population: ${country[0].population} </p>
+<p>Languages: ${country[0].languages[0].name}</p>`;
+}
